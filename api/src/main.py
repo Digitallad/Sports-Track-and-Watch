86a0daed3eac6805325813f28a574aff167dd24d
DigitalLad <digitallad@pm.me>
2025-11-12 21:55:56 -0500

    Add DB config + SQLAlchemy engine + startup table creation

--- api/src/main.py
+++ api/src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
+from .db import Base, engine
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/healthz")
 def healthz():
